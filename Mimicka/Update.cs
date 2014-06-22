a03e5819def81b292891398de0e207b272928a76
caellel <andre.pinto.x@gmail.com>
2014-06-21 20:02:14 -0400

    Fixing updating for case where the meta table didnt exist before

--- Mimicka/Update.cs
+++ Mimicka/Update.cs
@@ -29,26 +29,30 @@ namespace Tomestone
             var queryResult = Db.Query("SELECT * FROM @TableName", parms);
 
             if (queryResult.Rows.Count != 0) databaseVersion = queryResult.Rows[0]["version"].ToString();
-            else databaseVersion = "0";
-            
-            
+            else
+            {
+                // add new version entry to meta
+                var data = new Dictionary<string, string>();
+                data.Add("version", "100");
+                var results = Db.Insert("meta", data);
+                return true;
+            }
             // compare versions and update case by case
 
             switch (databaseVersion)
             {
-                case "0": // previously uninitialized version
+                case "0": 
                     
                     // fix the database
                     parms = new Dictionary<string, string>();
                     parms.Add("@TableName", "users");
                     parms.Add("@newColumn", "characterCount");
-                    Db.Query("ALTER TABLE @TableName ADD @newColumn NOT NULL DEFAULT ( '0' ) ;", parms); // TODO: handle a fail case where you try to add a column that already exists
+                    Db.Query("ALTER TABLE @TableName ADD @newColumn NOT NULL DEFAULT ( '0' )", parms); // TODO: handle a fail case where you try to add a column that already exists
 
                     // add new version entry to meta
                     var data = new Dictionary<string, string>();
                     data.Add("version", "100");
-                    var results = Db.Insert("meta", data);
-
+                    WriteToDatabase(data, databaseVersion);
                     break;
                 default:
                     break;
